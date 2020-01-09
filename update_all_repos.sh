91324bd969b52c5453ba8f3f93ff5e176c480262
Enrico Piazza <enry2077@gmail.com>
2020-01-09 19:14:49 +0100

    added collors to update script

--- update_all_repos.sh
+++ update_all_repos.sh
@@ -1,13 +1,15 @@
+END_COLOR="\e[0m"
+BLUE="\e[34m"
 
 EUROBENCH_WORKSPACE_PATH=${EUROBENCH_WORKSPACE_PATH:="$HOME/catkin_ws"}  # set this env var if it is not already set
 
-echo "EUROBENCH_WORKSPACE_PATH: $EUROBENCH_WORKSPACE_PATH"
+echo -e "${BLUE}EUROBENCH_WORKSPACE_PATH: $EUROBENCH_WORKSPACE_PATH${END_COLOR}"
 
 for i in `find $EUROBENCH_WORKSPACE_PATH -name .git -type d`;  # for all git repo directories in the eurobench workspace, do git pull
     do (
-        echo ""
-        echo $i
         cd $i/..
+        echo ""
+        echo -e "${BLUE}Pulling in `pwd`${END_COLOR}"
         git pull;
     );
 done
