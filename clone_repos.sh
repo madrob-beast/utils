e5969de4f9897cdfc05a006b77499316b2fa40ed
Enri2077 <enry2077@gmail.com>
2021-01-18 10:28:14 +0100

    changed clone script

--- clone_repos.sh
+++ clone_repos.sh
@@ -7,7 +7,7 @@ echo -e "${BLUE}EUROBENCH_WORKSPACE_PATH: $EUROBENCH_WORKSPACE_PATH${END_COLOR}"
 
 cd $EUROBENCH_WORKSPACE_PATH/src
 
-for i in `cat $EUROBENCH_WORKSPACE_PATH/src/utils/madrob_repos_list`;  # for all git repo directories in the eurobench workspace, do git pull
+for i in `cat $EUROBENCH_WORKSPACE_PATH/src/utils/repos_list`;  # for all git repo directories in the eurobench workspace, do git pull
     do (
         echo ""
         echo -e "${BLUE}Cloning $i${END_COLOR}"
